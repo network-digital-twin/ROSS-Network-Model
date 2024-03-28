7b5429ab985b5182873a79bf6d196f26753b8eec
nan42 <zhangnan1701@gmail.com>
2024-03-28 11:08:37 +0800

    change path to the file in current project

--- model/network_main.c
+++ model/network_main.c
@@ -38,10 +38,10 @@ tw_lpid total_terminals= 1;
 tw_lpid total_switches = 5237;
 
 char *trace_path = "/home/nan42/codes-dev/ROSS-Network-Model/WL_generation/traces/lightweight/1000ms/trace_0_FLOW_THROUGHPUT-1250000__SIMULATION_TIME-1000000000__PAIRS_PER_SRC-1-0__MSG_SIZE-10000__PACKET_SIZE-1400__BANDWIDTH-1250000__PRIO_LEVELS-3";
-char *route_dir_path = "/home/nan42/zte/omnet-bench/src/zte_qos/simulations/dataset/zte/final_topology_0";
+char *route_dir_path = "/home/nan42/codes-dev/ROSS-Network-Model/WL_generation/topologies/final_topology_0";
 char *home_dir = "/home/nan42/codes-dev/ROSS-Network-Model";
 char *out_dir = NULL;
-char *partition_file = "/home/nan42/codes-dev/ROSS-Network-Model/partition/graph-for-metis.txt.part.2";
+char *partition_file = "/home/nan42/codes-dev/ROSS-Network-Model/partition/graph-for-metis-w-size.txt.part.8";
 
 int queue_capacity_0 = 5000000; // 5MB: ~3571 packets
 int queue_capacity_1 = 20000000; // 20MB: ~14285 packets
@@ -142,9 +142,10 @@ int network_main(int argc, char** argv, char **env)
 
     if(tw_nnodes() > 1) {
 
-        char str[100];
-        sprintf(str, "/home/nan42/codes-dev/ROSS-Network-Model/partition/graph-for-metis.txt.part.%d", tw_nnodes());
+        char str[150];
+        sprintf(str, "/home/nan42/codes-dev/ROSS-Network-Model/partition/graph-for-metis-w-v-e-weight-pkt.txt.part.%d", tw_nnodes());
         partition_file=&str;
+	printf("%s\n",partition_file);
     }
 #ifdef DEBUG
     // debug mode, need to attach a debugger after run
